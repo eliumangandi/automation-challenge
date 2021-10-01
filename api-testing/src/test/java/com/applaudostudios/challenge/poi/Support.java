b09cdbaac654487ec40be5ef2ef190ad3dd7e974
eliumangandi <eliumangandi@gmail.com>
2021-09-30 21:39:44 -0600

    changes in modules

---
+++ api-testing/src/test/java/com/applaudostudios/challenge/poi/Support.java
@@ -0,0 +1,35 @@
+package com.applaudostudios.challenge.poi;
+
+public class Support {
+    String url;
+    String text;
+
+    public Support(String url, String text) {
+        this.url = url;
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Support{" +
+                "url='" + url + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
